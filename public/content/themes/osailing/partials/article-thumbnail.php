f67401020c8ef34884c5f27864a468b5e1db286d
Ali SHAKER <srusht2012@gmail.com>
2022-05-06 15:21:56 +0200

    dynamiser image thumbnail

--- public/content/themes/osailing/partials/article-thumbnail.php
+++ public/content/themes/osailing/partials/article-thumbnail.php
@@ -1,7 +1,13 @@
 <article class="post post--excerpt">
-      <img class="post__image" src="https://picsum.photos/300/200?random=1">
-      <h3 class="post__title"><?php the_title();?></h3>
+      <?php
+      if (has_post_thumbnail(get_the_id())) {
+            echo '<img class="post__image" src=" ' . get_the_post_thumbnail_url() . ' ">';
+      }
+
+      ?>
+
+      <h3 class="post__title"><?php the_title(); ?></h3>
       <!--CI dessus, je peux faire the_title() ou echo get_the_title();-->
-      <p class="post__excerpt"><?=get_the_excerpt();?></p>
-      <a href="<?php echo get_the_permalink();?>" class="post__link">Lire la suite</a>
+      <p class="post__excerpt"><?= get_the_excerpt(); ?></p>
+      <a href="<?php echo get_the_permalink(); ?>" class="post__link">Lire la suite</a>
 </article>
